e66241aca959dbff1e8411bc9eca847667d33011
merveaymaz <mervetturk@gmail.com>
2024-08-15 23:15:06 +0200

    Add DependencyInjection classes for seperation of concerns.

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
